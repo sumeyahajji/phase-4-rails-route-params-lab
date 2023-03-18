517893d5e96290d49cd422f96d95f91d8c408681
+ <+>
2023-03-18 13:12:55 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :students, only: [:show, :index]
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+ 
+   
+
+  
 end
